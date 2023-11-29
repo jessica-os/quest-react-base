e4b6eb1cc6216d5699569754a20e7295b1f9bd09
Jessica Oliveira <elisa0js@gmail.com>
2023-11-29 10:14:45 -0300

    atualização titulo

--- src/components/paragrafo.jsx
+++ src/components/paragrafo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Paragrafo = ({ color, text }) => {
-return <p style={{ color: color }}>{text.toUpperCase()}</p>};
-
+return <p style={{ color: color }}>{text.toUpperCase()}</p>;
+};
 
 export default Paragrafo;
